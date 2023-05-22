59e9b83bd3d3550e8002698b7b95bcdda8b08785
valentinaaranggo <valea28@gmail.com>
2023-05-22 19:45:50 -0400

    0.1.0
    created model folder and added three  model files Thought.js, User.js, and index.js. Finished adding all the information for the user model included username, email, thoughts, and friends and created a virtual.

---
+++ models/User.js
@@ -0,0 +1,43 @@
+const {Schema, model}= require('mongoose');
+
+const userSchema= new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trimmed: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            // regex
+            match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/]
+        },
+        thoughts: {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought',
+        },
+        friends: {
+            type:Schema.type.ObjectId,
+            ref:'User',
+        },
+    },
+    { 
+        toJSON:{
+            virtuals:true,
+            getters: true,
+        },
+        id:false,
+        }
+
+
+);
+
+userSchema.virtual('friendCount').get(function(){
+    return `${this.friends.length}`
+});
+const User= model('user', userSchema)
+
+module.exports= User;
